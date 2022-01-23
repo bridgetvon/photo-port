2e159663ec504cd685df6779f823354317a26f7a
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2022-01-22 17:25:21 -0700

    starting files

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+const About = () => {
+    return(
+        // React.createElement("div", {}, [
+        //     React.createElement("h1", {}, "About Me"),
+        //     React.createElement("p", {}, "some text")
+        // ]);
+        <section className='my-5'>
+            <h1 id="about"> Who am I? </h1>
+            <img src={coverImage} className='my-2' style={{ width: "100%"}} alt='cover'/>
+        </section>
+    )
+}
+
+export default About;
